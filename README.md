b0bc2e951aecfc1828c562d8aac6e9a58bed2d42
Laurence Tratt <laurie@tratt.net>
2019-05-08 14:48:46 +0100

    A first version of the benchmark runner.
    Renaissance outputs CSV in the form:
    
      benchmark,nanos,unixts.after,unixts.before
      rx-scrabble,459037012,1557301509752,1557301509293
      rx-scrabble,238308898,1557301509991,1557301509752
    
    We therefore end up with 1 file per pexec. convert_to_krun.py takes several
    pexec files and combines them into one CSV in Krun CSV format so that they
    can be processed with warmup_stats.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Renaissance benchmark reproduction
+
+In order to run:
+
+```sh
+$ ./run_benchmarks.sh
+```
---
+++ run_benchmarks.sh
@@ -0,0 +1,46 @@
+#! /bin/sh
+
+# Copyright (c) 2019 King's College London created by the Software Development Team
+# <http://soft-dev.org/>
+#
+# Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+# http://www.apache.org/licenses/LICENSE-2.0>, or the MIT license <LICENSE-MIT or
+# http://opensource.org/licenses/MIT>, or the UPL-1.0 license <http://opensource.org/licenses/UPL>
+# at your option. This file may not be copied, modified, or distributed except according to those
+# terms.
+
+set -e
+
+RENAISSANCE_V="0.9.0"
+GRAALCE_V="1.0.0-rc16"
+# This is the full list of benchmarks, but some of these are very slow
+# BENCHMARKS="actors,apache-spark,database,jdk-concurrent,jdk-streams,neo4j,rx,scala-dotty,scala-stdlib,scala-stm,twitter-finagle"
+# The benchmark list we're using below are the somewhat faster ones
+BENCHMARKS="chi-square,future-genetic,gauss-mix,naive-bayes,rx-scrabble,scala-stm-bench7,scala-kmeans,scrabble"
+PEXECS=10
+IPITERS=2000
+
+if [ ! -f renaissance-gpl-${RENAISSANCE_V}.jar ]; then
+    wget https://github.com/renaissance-benchmarks/renaissance/releases/download/v${RENAISSANCE_V}/renaissance-gpl-${RENAISSANCE_V}.jar
+fi
+
+if [ ! -d graalvm-ce-${GRAALC_V} ]; then
+    wget https://github.com/oracle/graal/releases/download/vm-${GRAALCE_V}/graalvm-ce-${GRAALCE_V}-linux-amd64.tar.gz
+    tar xfz graalvm-ce-${GRAALCE_V}-linux-amd64.tar.gz
+fi
+
+i=0
+while [ $i -lt ${PEXECS} ]; do
+    # The following JVM invocations are adopted from those detailed in:
+    #  https://github.com/renaissance-benchmarks/measurements/blob/7d3f09e05df3c5477fabe531ec5effc07e33f7aa/README.md
+
+    # Graal CE running the normal HotSpot compiler
+    graalvm-ce-${GRAALCE_V}/bin/java -Xms12G -Xmx12G -XX:-EnableJVMCI -XX:-UseJVMCICompiler -jar renaissance-gpl-${RENAISSANCE_V}.jar -r ${IPITERS} --csv openjdk-pexec-${i}.csv $BENCHMARKS
+
+    # Graal CE
+    graalvm-ce-${GRAALCE_V}/bin/java -Xms12G -Xmx12G -jar renaissance-gpl-${RENAISSANCE_V}.jar -r ${IPITERS} --csv graalvm-ce-pexec-${i}.csv $BENCHMARKS
+    i=$(($i + 1))
+done
+
+./convert_to_krun.py -o openjdk.csv openjdk-pexec*.csv
+./convert_to_krun.py -o graalvm-ce.csv graalvm-ce-pexec*.csv
