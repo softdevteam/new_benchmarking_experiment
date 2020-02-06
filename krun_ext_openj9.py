f33aa6a4acb7cc7b365ef101a91300fb62869982
Edd Barrett <vext01@gmail.com>
2020-02-06 17:11:26 +0000

    All OpenJ9 experiments were actually running GraalCE.

--- krun_ext_openj9.py
+++ krun_ext_openj9.py
@@ -9,4 +9,4 @@ from krun_ext_common import run, emit_process_exec_json
 
 
 _, benchmark, num_iters, param, instr = sys.argv
-emit_process_exec_json(run("graal-ce", benchmark, int(num_iters)))
+emit_process_exec_json(run("openj9", benchmark, int(num_iters)))
